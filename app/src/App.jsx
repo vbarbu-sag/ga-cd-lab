3c5368527fd02a158432116bf4c9e9a1130dc411
Vlad A. Barbu <vlad.barbu@sag-ag.ch>
2025-03-24 17:18:31 +0200

    init

---
+++ app/src/App.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+const baseUrl = "http://localhost:5236";
+
+async function getWeatherForecast() {
+  try {
+    const resp = await fetch(`${baseUrl}/weatherforecast`);
+    const forecast = await resp.json();
+    return forecast;
+  } catch (e) {
+    console.error(`oops something went wrong: ${e.message}`);
+    return [];
+  }
+}
+
+function ForecastRow({ date, temperatureC, summary }) {
+  const formattedDate = new Date(date).toLocaleDateString();
+  return (
+    <tr>
+      <td> {formattedDate} </td>
+      <td> {temperatureC} </td>
+      <td> {summary ?? '-'} </td>
+    </tr>
+  );
+}
+
+export default function App() {
+  const [forecast, setForecast] = useState([]);
+  useEffect(() => {
+    getWeatherForecast().then(setForecast);
+  }, []);
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Temperature (C)</th>
+            <th>Summary</th>
+          </tr>
+        </thead>
+        <tbody>
+          {forecast.map((x, index) => (
+            <ForecastRow key={index} {...x} />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
