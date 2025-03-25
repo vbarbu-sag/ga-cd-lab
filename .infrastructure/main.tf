222d2925cb665a2c4b301967b8e4b4262394e67f
Vlad A. Barbu <vlad.barbu@sag-ag.ch>
2025-03-25 16:36:40 +0200

    ensure azure doesn't try to run a build process on the server side since we're providing pre-built files

--- .infrastructure/main.tf
+++ .infrastructure/main.tf
@@ -49,20 +49,18 @@ resource "azurerm_linux_web_app" "app" {
   service_plan_id     = azurerm_service_plan.asp.id
   https_only          = true
   resource_group_name = azurerm_resource_group.rg.name
-
   app_settings = {
     "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "true"
     "API_URL"                             = "https://${azurerm_linux_web_app.api.default_hostname}"
+    "WEBSITE_WEBDEPLOY_USE_SCM"           = "false"
   }
-
   site_config {
     minimum_tls_version = "1.2"
     always_on           = false
-
     application_stack {
       node_version = "22-lts"
     }
-
+    app_command_line = "pm2 serve /home/site/wwwroot --no-daemon --spa"
     cors {
       allowed_origins = ["https://${azurerm_linux_web_app.api.default_hostname}"]
     }
