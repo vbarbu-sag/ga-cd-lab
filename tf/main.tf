d2dcb3a15efc788fb7d9d1613c1acf53dfc7b136
Vlad A. Barbu <vlad.barbu@sag-ag.ch>
2025-03-25 10:58:04 +0200

    adjust terraform module with api definition

--- tf/main.tf
+++ tf/main.tf
@@ -17,3 +17,24 @@ resource "azurerm_resource_group" "rg" {
   name     = "ga-cd-lab"
   location = "westeurope"
 }
+
+resource "azurerm_service_plan" "asp" {
+  name                = "ga-cd-lab-asp"
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Linux"
+  sku_name            = "F1"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_linux_web_app" "api" {
+  name                = "ga-cd-lab-api"
+  location            = azurerm_resource_group.rg.location
+  service_plan_id     = azurerm_service_plan.asp.id
+  https_only          = true
+  resource_group_name = azurerm_resource_group.rg.name
+
+  site_config {
+    minimum_tls_version = "1.2"
+    always_on           = false
+  }
+}
